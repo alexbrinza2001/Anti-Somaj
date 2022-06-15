687b619b44c547e5395daebcef3b884225813989
cimuletz <alex.serafim@s.unibuc.ro>
2022-06-15 23:31:30 +0300

    refactored user class

--- Backend/src/main/java/com/project/backend/entity/User.java
+++ Backend/src/main/java/com/project/backend/entity/User.java
@@ -1,8 +1,19 @@
 package com.project.backend.entity;
 
+import com.project.backend.entity.Employer;
+import com.project.backend.entity.Freelancer;
+import com.project.backend.entity.Role;
+import lombok.Data;
+
 import javax.persistence.*;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
+import static javax.persistence.FetchType.EAGER;
+
 @Entity
+@Data
 @Table(name = "user")
 public class User {
 
@@ -14,9 +25,19 @@ public class User {
     private String firstName;
     private String lastName;
     private String password;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "freelancer_id")
+    private Freelancer freelancer;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "employer_id")
+    private Employer employer;
+
+    @ManyToMany(fetch = EAGER)
+    private Collection<Role> roles = new ArrayList<>();
     private Integer employerId;
     private Integer freelancerId;
-
 
     public Integer getUserId() {
         return userId;
@@ -58,6 +79,22 @@ public class User {
         this.password = password;
     }
 
+    public Freelancer getFreelancer() {
+        return freelancer;
+    }
+
+    public void setFreelancer(Freelancer freelancer) {
+        this.freelancer = freelancer;
+    }
+
+    public Employer getEmployer() {
+        return employer;
+    }
+
+    public void setEmployer(Employer employer) {
+        this.employer = employer;
+    }
+
     public Integer getEmployerId() {
         return employerId;
     }
@@ -72,5 +109,14 @@ public class User {
 
     public void setFreelancerId(Integer freelancerId) {
         this.freelancerId = freelancerId;
+    }
+
+
+    public Collection<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Collection<Role> roles) {
+        this.roles = roles;
     }
 }
--- Backend/src/main/java/com/project/backend/service/UserService.java
+++ Backend/src/main/java/com/project/backend/service/UserService.java
@@ -11,13 +11,20 @@ import com.project.backend.repo.EmployerRepo;
 import com.project.backend.repo.FreelancerRepo;
 import com.project.backend.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     @Autowired
     UserRepo userRepository;
@@ -65,6 +72,19 @@ public class UserService {
         }
 
         return users;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if(user == null){
+            throw new UsernameNotFoundException("User not found in database");
+        }
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+        user.getRoles().forEach(role -> {
+            authorities.add(new SimpleGrantedAuthority(role.getName()));
+        });
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), authorities);
     }
 
 }
