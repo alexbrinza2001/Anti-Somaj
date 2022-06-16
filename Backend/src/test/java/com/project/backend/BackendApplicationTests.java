3f13bf7f10527de495e00d9c7d5991780a65e7e9
Alexandru Brînză <72472437+alexbrinza2001@users.noreply.github.com>
2022-06-16 06:10:12 +0300

    Small update

--- Backend/src/test/java/com/project/backend/BackendApplicationTests.java
+++ Backend/src/test/java/com/project/backend/BackendApplicationTests.java
@@ -56,6 +56,7 @@ class BackendApplicationTests {
     }
 
     @Test
+    @Transactional
     void checkPasswordTooShort() throws Exception {
 
         User user = new User();
@@ -103,6 +104,7 @@ class BackendApplicationTests {
     }
 
     @Test
+    @Transactional
     void checkNameContainsNumber() throws Exception {
 
         Employer employer = new Employer();
@@ -116,6 +118,7 @@ class BackendApplicationTests {
     }
 
     @Test
+    @Transactional
     void checkRatingValue() throws Exception {
 
         Review review = new Review();
